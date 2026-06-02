41b279b87d2b18b50b4fee67e3021d6fa8e128ea
Owen Fonseca Asenie <owiinaasenie@gmail.com>
2026-06-02 05:08:34 +0000

    Try/Except (AGREGADOS)

--- main.py
+++ main.py
@@ -1,24 +1,37 @@
 while True:
-    # Mostrar menu
-    print("Selecciona la opcion que deseas realizar")
-    print("1- Calcular comision")
-    print("2- Ver recibos de sueldo de vendedores (Admin)")
-    print("3- Salir")
-    opc = int(input("Opcion: "))
+    try:
+        # Mostrar menu
+        print("|---------------------------------------|")
+        print("Selecciona la opcion que deseas realizar")
+        print("|---------------------------------------|")
+        print("1- Calcular comision")
+        print("2- Ver recibos de sueldo de vendedores (Admin)")
+        print("3- Salir")
+        opc = int(input("Opcion: "))
 
-    # Ralizar accion del menu
-    match opc:
-        case 1:
-            print("|------------------|")
-            print("calcular comision")
-            print("|------------------|")
-        case 2:
-            print("|------------------|")
-            print("Ver recibos")
-            print("|------------------|")
-        case 3:
-            print("|------------------|")
-            print("Saliendo...")
-            print("|------------------|")
-            exit()
+        # Realizar accion del menu
+        match opc:
+            case 1:
+                print("|------------------|")
+                print("calcular comision")
+                print("|------------------|")
 
+            case 2:
+                print("|------------------|")
+                print("Ver recibos")
+                print("|------------------|")
+
+            case 3:
+                print("|------------------|")
+                print("Saliendo...")
+                print("|------------------|")
+                break
+
+            case _:
+                print("|------------------|")
+                print("Error: Debes ingresar una opcion valida (1, 2 o 3)")
+                print("|------------------|")
+
+    except ValueError:
+        print("Error: No se permiten letras ni numeros decimales.")
+        print("Por favor, ingresa un numero entero valido.")
