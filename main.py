84c29ca403ac6dd8297d868ae9a3b4279f952532
Owen Fonseca Asenie <owiinaasenie@gmail.com>
2026-06-02 05:33:33 +0000

    Try/Except (Modificado AGREGADO)

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 while True:
     try:
         # Mostrar menu
-        print("|---------------------------------------|")
         print("Selecciona la opcion que deseas realizar")
-        print("|---------------------------------------|")
         print("1- Calcular comision")
         print("2- Ver recibos de sueldo de vendedores (Admin)")
         print("3- Salir")
+
         opc = int(input("Opcion: "))
 
         # Realizar accion del menu
         match opc:
             case 1:
                 print("|------------------|")
-                print("calcular comision")
+                print("Calcular comision")
                 print("|------------------|")
 
             case 2:
@@ -28,10 +27,8 @@ while True:
                 break
 
             case _:
-                print("|------------------|")
-                print("Error: Debes ingresar una opcion valida (1, 2 o 3)")
-                print("|------------------|")
+                print("Error: Debes ingresar una opcion valida (1, 2 o 3).")
 
     except ValueError:
-        print("Error: No se permiten letras ni numeros decimales.")
-        print("Por favor, ingresa un numero entero valido.")
+        print("Error: Debes ingresar un numero entero.")
+        print("Error: Debes ingresar una opcion valida (1, 2 o 3).")
